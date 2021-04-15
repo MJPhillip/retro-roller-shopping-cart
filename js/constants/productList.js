a8fc437904aa3a866d72ce8de56cb61569ae0353
MJ Phillip <michael.john.phillip@gmail.com>
2021-04-15 16:09:33 +0100

    Added files

---
+++ js/constants/productList.js
@@ -0,0 +1,23 @@
+export const productArray = [
+    {
+      name: "Bicyle",
+      description: "A really nice bicycle.",
+      image: "https://images.unsplash.com/photo-1571068316344-75bc76f77890?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YmljeWNsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      id: "1",
+      price: 400,
+    },
+    {
+      name: "Skateboard",
+      description: "A really nice skateboard.",
+      image: "https://images.unsplash.com/photo-1536318431364-5cc762cfc8ec?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2thdGVib2FyZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      id: "2",
+      price: 150,
+    },
+    {
+      name: "Rollerskates",
+      description: "A really nice rollerskates.",
+      image: "https://images.unsplash.com/photo-1595916102670-1b4f01a04dfa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cm9sbGVyc2thdGVzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      id: "3",
+      price: 250,
+    }
+  ];
